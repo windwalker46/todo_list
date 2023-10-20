6fc9d2f9f9c059416597e71b8a8e894187751c5f
Ben Tran <bambootran68@gmail.com>
2023-10-20 12:00:59 -0500

    Added todo_list content and other changes

---
+++ todo_list.py
@@ -0,0 +1,15 @@
+# Import the datetime module for handling date inputs and operations
+import datetime
+
+# This list will store all our tasks. Each task is a dictionary.
+tasks = []
+
+# This function displays the main menu of the app to the user.
+def display_menu():
+    print("\nTo-Do List App:")
+    print("1. Add a task")
+    print("2. List all tasks")
+    print("3. Mark a task as done")
+    print("4. Delete a task")
+    print("5. Exit")
+
